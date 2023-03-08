ca1c3c26b64882583be98a9bcc1ad15ecabe4d85
Sergei Vedishchev <brainboost@gmail.com>
2023-03-08 01:46:11 +0100

    tests

--- app.py
+++ app.py
@@ -2,9 +2,17 @@
 
 import aws_cdk as cdk
 from stacks.chatgpt_bot_stack import ChatgptBotStack
-from stacks.webhook_stack import WebhookStack
+
+# from stacks.webhook_stack import WebhookStack
 
 app = cdk.App()
 botStack = ChatgptBotStack(app, "ChatgptBotStack")
-webhookStack = WebhookStack(app, "WebhookStack", botStack.lambda_layer)
+# webhookStack = WebhookStack(
+#     app,
+#     "WebhookStack",
+#     botStack.lambda_layer.layer_version_arn,
+#     botStack.lambda_function_url,
+#     botStack.bucket.bucket_name,
+# )
+# webhookStack.add_dependency(botStack)
 app.synth()
